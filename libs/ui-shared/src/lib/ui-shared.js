aebd668348bfb105b976028d7e2fbf64bbcc81ac
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2020-11-19 08:57:36 -0500

    Adding ui shared library.

---
+++ libs/ui-shared/src/lib/ui-shared.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ui-shared.scss';
+export function UiShared(props) {
+  return (
+    <div>
+      <h1>Welcome to ui-shared!</h1>
+    </div>
+  );
+}
+export default UiShared;
